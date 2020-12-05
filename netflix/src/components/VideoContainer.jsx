3c2831770e5fa15654a99e1a9fd788056a91ee0b
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-12-05 17:26:23 +0100

    create axios fetch in videoContainer

--- netflix/src/components/VideoContainer.jsx
+++ netflix/src/components/VideoContainer.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
 import VideoItem from "./VideoItem"
 
 const VideoContainer = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(async() => {
+        const result = await axios(
+            'http://localhost:5000/videos',
+        );
+        setData(result.data);
+},[setData])
+
     return(
         <div>
-            <VideoItem url="https://www.youtube.com/watch?v=Q8Tiz6INF7I" title="Hit the road Jack" id="1"/>   
-            <VideoItem url="https://www.youtube.com/watch?v=Wr9ie2J2690" title="Deep Purple: Highway Star" id="2"/>      
-            <VideoItem url="https://www.youtube.com/watch?v=jhK2ev_O-pc" title="Powerwolf:Demons are a girls best friend" id="3"/>
+        {data.map(video => (
+            <div>
+                <VideoItem title={video.title} id={video.id} url={video.url} />
+            </div>
+            ))}
         </div>
-    )
+    );
 }
 export default VideoContainer;
